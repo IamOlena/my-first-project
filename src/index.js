e12503457da2397cb3272d9efc212ace700a8ee0
IamOlena <110056881+IamOlena@users.noreply.github.com>
2022-08-20 19:40:14 +0200

    integrated API to forecast

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ function displayTime(date) {
       "Friday",
       "Saturday"
     ];
-    let day = weekDays[currentDate.getDay()];
+    let day = weekDays[date.getDay()];
     let hour = date.getHours();
     let min = date.getMinutes();
   
@@ -24,30 +24,50 @@ let h2 = document.querySelector("h2");
   let currentDate = new Date();
   h2.innerHTML = displayTime(currentDate);
 
-  function displayForecast(){
+function formatDay(timestamp){
+  let date= new Date(timestamp*1000);
+  let day = date.getDay();
+  let days = [
+    "Sun",
+    "Mon",
+    "Tue",
+    "Wed",
+    "Thu",
+    "Fri",
+    "Sat"
+  ];
+  return days[day];
+}
+
+  function displayForecast(response){
     let forecastElement = document.querySelector(".forecast");
-    let days = ["Sun","Mon","Tue","Wen","Thu"];
-     
+    let forecast= response.data.daily;
     let forecastHTML = `<div class = "row">`;
-    days.forEach(function (day) {
+    forecast.forEach(function (forecastDay, index) {
+      if (index<6){ 
      forecastHTML= forecastHTML + `
         <div class="col-2">
-        <div class="forecast-day">${day}</div>
-        <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="" width="50">
+        <div class="forecast-day">${formatDay(forecastDay.dt)}</div>
+        <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="50">
       <div class="forecast-temperatures">
-<span class="forecast-temp-max">25°</span>
-<span class="forecast-temp-min">17°</span>
+<span class="forecast-temp-max">${Math.round(forecastDay.temp.max)}°</span>
+<span class="forecast-temp-min">${Math.round(forecastDay.temp.min)}°</span>
       </div>
     </div>
-     `;
+     `; };
     });
 
     forecastHTML= forecastHTML + `</div>`;
     forecastElement.innerHTML = forecastHTML;
-
+    
   }
   
-  
+  function receiveForecast(coordinates){
+    let apiKey = "c12144c45d93a92cd906b74d7711e356";
+    let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+  console.log(apiUrl);
+  }
   
   function showTemperature(response) {
     celsiusTemp = response.data.main.temp;
@@ -64,7 +84,9 @@ let h2 = document.querySelector("h2");
     let iconElement = document.querySelector("#icon");
     iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   iconElement.setAttribute("alt", response.data.weather[0].description);
-  
+   console.log(response.data.coord);
+   receiveForecast(response.data.coord);
+
   }
   function search(city) {
     let units = "metric";
@@ -117,5 +139,5 @@ let h2 = document.querySelector("h2");
   let celsiusTemp = null;
 
   search("Stockholm");
-  displayForecast();
+  
   
